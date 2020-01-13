71e152e8463cb5d04f7023defb19d771508f88f3
Jaenne <59830376+Jaenne@users.noreply.github.com>
2020-01-13 15:06:59 +0100

    Update fonctions_mastemind.py

--- fonctions_mastemind.py
+++ fonctions_mastemind.py
@@ -2,7 +2,7 @@
 # -*-coding:Utf-8 -*
 
 """
-Toutes les fonctions du Mastermind.
+All the functions of the Mastermind.
 """
 
 import os
@@ -10,15 +10,16 @@ import pickle
 from random import randrange
 from package.donnees import *
 os.chdir(".")
+
 """
-Fonctions de menu --------------------------------------------------------------------
+Menu functions --------------------------------------------------------------------
 """
 def ouvrir_le_fichier_sauvegardes():
 	"""
-	Cette fonction vérifie que le fichier "sauvegardes" existe
-	si oui, elle renvoie le contenu dans la fonction main.
-	si non, elle créer le fichier "sauvegarde" avec un tableau des scores vide.
-		puis elle renvoie le contenu dans la fonction main.
+	This function checks that the "backups" file exists
+	if so, it returns the content in the main function.
+	if not, it creates the "backup" file with an empty scoreboard.
+		then it returns the content in the main function.
 	"""
 	if os.path.exists(fichier_de_sauvegardes):
 		with open("sauvegardes", "rb") as fichier:
@@ -33,11 +34,11 @@ def ouvrir_le_fichier_sauvegardes():
 
 def ouvrir_la_sauvegarde(nom_du_joueur):
 	"""
-	Cette fonction ouvre le fichier "sauvegardes"
-	puis vérifie que le joueur a une sauvegarde à son nom.
-	si oui, elle renvoie le score du joueur dans la fonction main.
-	si non, elle créer une sauvegarde au nom du joueur avec 0 point.
-		puis renvoie le score du joueur dans la fonction main.
+	This function opens the "backups" file
+	then verify that the player has a save in his name.
+	if so, it returns the player's score in the main function.
+	if not, it will create a backup on behalf of the player with 0 points.
+		then returns the player's score in the main function.
 	"""
 	with open("sauvegardes", "rb") as fichier:
 		mon_depickler = pickle.Unpickler(fichier)
@@ -55,10 +56,10 @@ def ouvrir_la_sauvegarde(nom_du_joueur):
 
 def enregistrer_le_score(nom_du_joueur, score_du_joueur):
 	"""
-	La fonction vérifie si le score du joueur est supérieur à son ancien meilleur 
+	The function checks if the player's score is higher than his former best
 	score.
-	Si oui, elle écrase l'ancien score et enregistre le nouveau meilleur score.
-	si non, elle ne touche pas au fichier de sauvegarde.
+	If so, it overwrites the old score and records the new best score.
+	if not, it does not touch the backup file.
 	"""
 	with open("sauvegardes", "rb") as fichier:
 		mon_depickler = pickle.Unpickler(fichier)
@@ -72,54 +73,51 @@ def enregistrer_le_score(nom_du_joueur, score_du_joueur):
 						tableau_des_scores[nom_du_joueur] = score_du_joueur
 						tableau_des_scores = mon_pickler.dump(tableau_des_scores)
 						print(
-							"Vous avez battu votre ancien score ! \nVous aviez", 
-							ancien_score, "point(s) et maintenant vous avez",
-							 score_du_joueur, "point(s).\nScore sauvegardé."
+							"You beat your old score! \ nYou had ", 
+							ancien_score, "point (s) and now you have",
+							 score_du_joueur, "point(s).\nScore saved."
 							 )
 				elif ancien_score == score_du_joueur:
 					print(
-						"Vous avez égallé votre ancien score ! \nVous avez",
+						"You have matched your old score! \ nYou have",
 						score_du_joueur, "point(s)."
 						)
 				else:
 					print(
-						"Votre meilleur score est :", 
+						"Your best score is :", 
 						ancien_score
 						)
 
 def sortir():
-	quitter = input("Pour quitter taper Q:")
+	quitter = input("To quit type Q :")
 	if quitter == "q" or quitter == "Q" :
 		return True
 	else:
 		return False
 
 """
-Fonctions de gestion des saisies utilisateur------------------------------------------
+User input management functions------------------------------------------
 """
 def verifier_saisie():
-	""" La saisie du joueur doit comporter 4 Lettres.
-	Ces lettres sont stockées dans la liste autorisée.
-	La saisie est convertie capitales.
-	Si la saisie ne convient pas, car il y trop de caracteres
-	ou qu'il ne s'agit pas de caractères alpha, la fonction
-	recommence au début.
-	Chaque lettre de la saisie est stockée dans la liste
-	combinaison.
-	Si la saisie ne convient pas, car les lettres ne sont
-	pas dans la liste autorisée, la fonction recommence au
-	début.
-	Si la saisie convient, elle est renvoyée dans la
-	fonction main.
+	""" The player must enter 4 letters.These letters are stored 
+	in the authorized list.
+	The entry is converted capitals.If the entry is not suitable, 
+	because there are too many charactersor that they are not alpha 
+	characters, 
+	the function start over at the beginning.Each letter of the entry 
+	is stored in the listcombination.
+	If the entry is not suitable, because the letters are notnot in 
+	the authorized list, the function starts again at beginning.
+	If the entry is suitable, it is returned to the main function.
 	"""
 	autorisee = ["A", "B", "C", "D", "E", "F", "H", "I"]
 	combinaison = ["", "", "", ""]
 	saisie = input(
-		"Choisissez votre combinaison parmi ces lettres : A B C D E F H I :"
+		"Choose your combination among these letters : A B C D E F H I :"
 		)
 	if len(saisie) != 4 or not saisie.isalpha():
 		print(
-			"Votre combinaison doit comporter 4 caractères parmi ceux proposés."
+			"Your combination must have 4 letters among those proposed."
 			)
 		return verifier_saisie()
 	else :
@@ -129,23 +127,23 @@ def verifier_saisie():
 			for j, lettre in enumerate(saisie):
 				if saisie[j] not in autorisee:
 					print(
-						saisie[j], "n'est pas autorisée."
+						saisie[j], "is not allowed."
 						 )
 					return verifier_saisie()
 	return combinaison
 	
 """
-Fonctions du corps du jeu ------------------------------------------------------------
+Functions of the game ------------------------------------------------------------
 """
 
 def generer_code_sans_doubles():
-	""" Le code est une liste de "x".
-	Chaque x prend une valeur ente 0 et 7.
-	S'il y a un double, la fonction recommence au début.
-	Lorsque le code ne contient que des chiffres,
-	Chaque chiffre est convertit en lettre parmi celles
-	autorisées.
-	Renvoie le code dans la fonction main.
+	""" The code is a list of "x".
+	Each x takes a value between 0 and 7.
+	If there is a double, the function starts again from the beginning.
+	When the code contains only numbers,
+	Each number is converted into a letter among those
+	authorized.
+	Returns the code in the main function.
 	"""
 	code = ["x", "x", "x", "x"]
 	for i, element in enumerate(code):
@@ -174,16 +172,16 @@ def generer_code_sans_doubles():
 	return code
 
 def comparaison_couleurs(combinaison, code, compteur_d_essais):
-	""" Fonction qui compare le code et la combinaison du joueur
-	en cherchant les correspondances de couleur.
-	- sauvegarde la combinaison ;
-	- Remplace les doubles par un "x" ;
-	- compare la combinaison sans doubles avec le code ;
-	- incrémente de 1 le compteur d'essais 
-	- renvoie dans la fonction main :
-		- le nombre de bonnes couleurs ;
-		- le compteur d'essais ;
-		- la sauvegarde de la combinaison.
+	""" Function which compares the code and the combination of the player
+	by looking for color matches.
+	- save the combination;
+	- Replaces duplicates with an "x";
+	- compare the combination without duplicates with the code;
+	- increments the test counter by 1
+	- returns in the main function:
+		- the number of good colors;
+		- the test counter;
+		- saving the combination.
 	"""
 	bonnes_couleurs = 0
 	sauvegarde_combinaison = []
@@ -198,10 +196,10 @@ def comparaison_couleurs(combinaison, code, compteur_d_essais):
 	return bonnes_couleurs, compteur_d_essais, sauvegarde_combinaison
 
 def comparaison_places(tableau_combinaison, tableau_code):
-	""" Fonction qui cherche les couleurs bien placées: 
-	Compare les couleurs pour chaque index similaires du
-	tableau combinaison et du tableau code.
-	Retourne le nombre de couleurs bien placées. """
+	""" Function that searches for well-placed colors:
+	Compare the colors for each similar index of the
+	combination table and code table.
+	Returns the number of well-placed colors. """
 	bonnes_places = 0
 	for k, v in tableau_combinaison.items():
 		for key, value in tableau_code.items():
@@ -211,11 +209,17 @@ def comparaison_places(tableau_combinaison, tableau_code):
 	return bonnes_places
 
 def transformer_en_tableau(liste):
+	"""
+	Transform the list into a dictionary
+	"""
 	tableau_liste = {}
 	for i, lettre in enumerate(liste):
 		tableau_liste[i] = lettre
 	return tableau_liste
 
 def afficher_tableau(tableau, texte):
+	"""
+	Show dictionary (not used)
+	"""
 	for k, v in tableau.items():
 		print(texte, ": {} : {}".format(k,v))
