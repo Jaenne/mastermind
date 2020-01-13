870c5c3836e802603704f1421a5fc459e2469e64
Jaenne <59830376+Jaenne@users.noreply.github.com>
2020-01-13 14:47:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,69 +6,68 @@ import os
 os.chdir(".")
 
 """
-Dans Mastermind vous devez trouver le code.
-Il fut inventé par Mordecai Meirowitz.
-Vous avez 12 essais.
-Le nombre de points dépend du nombre d'essais restant.
-lors d'un essai vous proposez un code de 4 couleur.
-Le code ne peut pas contenir plusieurs fois la même couleur.
-Le code ne peut être composé de vide.
-Le jeu repondra quelles couleurs sont bien dans le codes
-et quelles couleurs sont à la bonnes places.
-couleurs dans le codes = (couleurs dans le code - couleurs à la bonne place).
-les couleurs seront remplacées par des lettres : A B C D E F H I
-Les bonnes couleurs à la mauvaise place sont symbolisées par : X
-Les bonnes couleurs à la bonne place sont symbolisées par : V
+In Mastermind you have to find the code.
+It was invented by Mordecai Meirowitz.
+You have 12 tries.
+The number of points depends on the number of tries remaining.
+during a test you offer a 4 color code.
+The code cannot contain the same color more than once.
+The code cannot be made up of blanks.
+The game will answer which colors are in the codes
+and which colors are in the right places.
+colors in the code = (colors in the code - colors in the right place).
+colors will be replaced by letters: A B C D E F H I
+The right colors in the wrong place are symbolized by: X
+The right colors in the right place are symbolized by: V
+
 """
 
 # Message d'accueil.
-print("Bienvenue dans Mastermind.")
-nom = input("Quel est votre nom ? ")
-phrase_1 = "X signifie : bonne couleur MAL placée.\n"
-phrase_2 = "V signifie : bonne couleur BIEN placée.\n"
-phrase_3 ="Le code se compose de 4 lettres, sans doublons.\n"
-phrase_4 = "Vous avez 12 essais.\n"
+print("Welcome in Mastermind.")
+nom = input("What's your nam ? ? ")
+phrase_1 = "X means : right color BAD place.\n"
+phrase_2 = "V means : right color Good place.\n"
+phrase_3 ="The code consists of 4 letters, without duplicates.\n"
+phrase_4 = "You have 12 tries.\n"
 print(phrase_1 + phrase_2 + phrase_3 + phrase_4)
 
 # Ouvrir le fichier score.
 tableau_des_scores = ouvrir_le_fichier_sauvegardes()
 score = ouvrir_la_sauvegarde(nom)
-print("Votre meilleur score :", score)
+print("Your best score :", score)
 print(
-	"\n\nX : MAL placée(s)", 
+	"\n\nX : BAD place(s)", 
 	"				",
-	"V : BIEN placée(s)\n", 
+	"V : GOOD place(s)\n", 
 	)
 
 quitter = False
 
-# Boucle de jeu.
+# Game loop.
 while not quitter:
 	compteur_d_essais = 0
 	bonnes_places = 0
 	compteur_d_essais = 0
 	code = generer_code_sans_doubles()
-	#print("Code sans doubles :\n", code)
+	#print("Code :\n", code)
 
-	# Boucle du code en cours de cassage.
+	# Secret code loop.
 	while bonnes_places < 4 and compteur_d_essais < 12:
 		combinaison = verifier_saisie()
 		bonnes_couleurs, compteur_d_essais, combinaison = comparaison_couleurs(
-																combinaison, 
-																code, 
-																compteur_d_essais
-																)
+										combinaison, 
+										code, 
+										compteur_d_essais
+										)
 		tableau_code = transformer_en_tableau(code)
 		tableau_combinaison = transformer_en_tableau(combinaison)
-		#afficher_tableau(tableau_code, "code")
-		#afficher_tableau(tableau_combinaison, "combinaison")
 		bonnes_places = comparaison_places(
-							tableau_combinaison, 
-							tableau_code 
-							)
+						tableau_combinaison, 
+						tableau_code 
+						)
 		bonnes_couleurs = (bonnes_couleurs - bonnes_places)
 
-		# Affiche en 1 ligne les informations nécessaires à la résolution du code.
+		# Displays in 1 line informations necessary for the resolution of the code.
 		print(
 			"X :", bonnes_couleurs, 
 			"			",
@@ -86,20 +85,20 @@ while not quitter:
 
 	if bonnes_places == 4:
 		print(
-			"Félictations, vous avez cassé le code en ",
+			"Congratulations, you broke the code in : ",
 			compteur_d_essais, 
-			"essais.\n Votre score est :",
+			"tries.\n Your score is : ",
 			score
 			)
 		enregistrer_le_score(nom, score)
 	else:
 		print(
-			"Vous n'avez pas cassé le code : \n		",
+			"You did not break the code : \n		",
 			code[0], 
 			code[1], 
 			code[2], 
 			code[3], 
-			"\nVotre score est :",
+			"\nYour score is :",
 			score
 			)
 
