20463f90b3d42d1ce0e823a1248dc0f6ea82d276
Jaenne <59830376+Jaenne@users.noreply.github.com>
2020-01-15 18:35:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,64 +10,66 @@ In Mastermind you have to find the code.
 It was invented by Mordecai Meirowitz.
 You have 12 tries.
 The number of points depends on the number of tries remaining.
-During a test you offer a 4 color code.
-The code cannot contain the same color more than once.
+during a test you offer a 4 color code.
 The code cannot be made up of blanks.
-The game will answer which colors are in the codes
+Normal level : The code cannot contain the same color more than once.
+Hard level : The code may contain duplicates.
+The game will answer which colors are in the code.
 and which colors are in the right places.
-Colors in the code = (colors in the code - colors in the right place).
-Colors will be replaced by letters: A B C D E F H I
+colors in the code = (colors in the code - colors in the right place).
+colors will be replaced by letters: A B C D E F H I
 The right colors in the wrong place are symbolized by: X
 The right colors in the right place are symbolized by: V
 
 """
 
-# Welcome text :
-print("Welcome in Mastermind.")
-nom = input("What's your name ? ")
-phrase_1 = "X means : right color BAD place.\n"
-phrase_2 = "V means : right color Good place.\n"
-phrase_3 ="The code consists of 4 letters, without duplicates.\n"
-phrase_4 = "You have 12 tries.\n"
-print(phrase_1 + phrase_2 + phrase_3 + phrase_4)
 
-# Open the score file.
+print("Welcome to Mastermind.")
+nom = input("What's your name ? ")
+print(message_accueil())
+
+# Ouvrir le fichier score.
 tableau_des_scores = ouvrir_le_fichier_sauvegardes()
 score = ouvrir_la_sauvegarde(nom)
-print("Your best score :", score)
-print(
-	"\n\nX : BAD place(s)", 
-	"				",
-	"V : GOOD place(s)\n", 
-	)
 
 quitter = False
 
-# Game loop.
+# Boucle de jeu.
 while not quitter:
 	compteur_d_essais = 0
 	bonnes_places = 0
 	compteur_d_essais = 0
-	code = generer_code_sans_doubles()
-	#print("Code :\n", code)
+	print("Choose your difficulty level.")
+	difficulte = choix_difficulte()
+
+	if difficulte == "hard":
+		code = generer_code()
+		#print("Code :\n", code)
+		print("The code consists of 4 letters. There may be duplicates.")
+	else:
+		code = generer_code_sans_doubles()	
+		#print("Code sans doubles :\n", code)
+		print("The code consists of 4 letters, without duplicates.\n")
+
+	print(
+		"\n\nX : BAD place(s)", 
+		"				",
+		"V : GOOD place(s)\n", 
+		)
 
 	# Secret code loop.
 	while bonnes_places < 4 and compteur_d_essais < 12:
 		combinaison = verifier_saisie()
 		bonnes_couleurs, compteur_d_essais, combinaison = comparaison_couleurs(
-										combinaison, 
-										code, 
-										compteur_d_essais
-										)
-		tableau_code = transformer_en_tableau(code)
-		tableau_combinaison = transformer_en_tableau(combinaison)
-		bonnes_places = comparaison_places(
-						tableau_combinaison, 
-						tableau_code 
-						)
+																combinaison, 
+																code, 
+																compteur_d_essais
+																)
+		bonnes_places = comparaison_places(combinaison, code)
 		bonnes_couleurs = (bonnes_couleurs - bonnes_places)
 
-		# Displays in 1 line informations necessary for the resolution of the code.
+		# Displays in 1 line the information necessary 
+		# for the resolution of the code.
 		print(
 			"X :", bonnes_couleurs, 
 			"			",
@@ -78,16 +80,18 @@ while not quitter:
 			"			",
 			"V :", bonnes_places, 
 			"		",
-			"essai n°", compteur_d_essais
+			"try n°", compteur_d_essais
 			)
-
-	score = (12 - compteur_d_essais)
+	if difficulte == "hard":
+		score = (12 - compteur_d_essais) * 2
+	else:
+		score = (12 - compteur_d_essais)
 
 	if bonnes_places == 4:
 		print(
-			"Congratulations, you broke the code in : ",
+			"\n\nCongratulations, you broke the code in",
 			compteur_d_essais, 
-			"tries.\n Your score is : ",
+			"tries.\nYour score is :",
 			score
 			)
 		enregistrer_le_score(nom, score)
@@ -102,4 +106,4 @@ while not quitter:
 			score
 			)
 
-	quitter = sortir()
+	quitter, nom = sortir(nom)
