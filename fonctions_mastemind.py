58dfe60ead0bfd33e7ce8a6e130b7235d23e73d5
Jaenne <59830376+Jaenne@users.noreply.github.com>
2020-01-23 19:31:13 +0100

    Update fonctions_mastemind.py

--- fonctions_mastemind.py
+++ fonctions_mastemind.py
@@ -11,23 +11,25 @@ os.chdir("package")
 """
 Menu functions --------------------------------------------------------------------
 """
+
 def open_backup_file():
 	"""
 	This function checks that the "backups" file exists
-	if so, it returns the content in the main function.
 	if not, it creates the "backup" file with an empty scoreboard.
-	then it returns the content in the main function.
 	"""
-	if os.path.exists("saves"):
-		with open("saves", "rb") as file:
-			my_depickler = pickle.Unpickler(file)
-			scoreboard = my_depickler.load()		
-	else:
+	if not os.path.exists("saves"):
 		with open("saves", "wb") as file:
-			my_pickler = pickle.Pickler(file)
 			scoreboard = {}
+			my_pickler = pickle.Pickler(file)
 			scoreboard = my_pickler.dump(scoreboard)
-	return scoreboard
+
+def write_backup(name, scoreboard):
+	with open("saves", "wb") as file:
+		player_score = 0
+		my_pickler = pickle.Pickler(file)
+		scoreboard[name] = player_score
+		scoreboard = my_pickler.dump(scoreboard)
+		return player_score
 
 def open_backup(name):
 	"""
@@ -35,7 +37,6 @@ def open_backup(name):
 	then verify that the player has a save in his name.
 	if so, it returns the player's score in the main function.
 	if not, it will create a backup on behalf of the player with 0 points.
-	then returns the player's score in the main function.
 	"""
 	with open("saves", "rb") as file:
 		my_depickler = pickle.Unpickler(file)
@@ -43,18 +44,10 @@ def open_backup(name):
 		for player, point in scoreboard.items():
 			if player == name:
 				player_score = point
-				print ("Hello ", name)
-				print("Your best score :", player_score)
 				return player_score
-	with open("saves", "wb") as file:
-		my_pickler = pickle.Pickler(file)
-		player_score = 0
-		scoreboard[name] = player_score
-		scoreboard = my_pickler.dump(scoreboard)
-	print ("Hello ", name)		
-	print("Your best score :", player_score)
-	return player_score
-
+		player_score = write_backup(name, scoreboard)
+		return player_score
+    
 def save_score(name, player_score):
 	"""
 	The function checks if the player's score is higher than his former 
@@ -90,6 +83,7 @@ def save_score(name, player_score):
 						old_score,
 						"\n"
 						)
+
 def compare_score(name, player_score):
 	"""
 	The function checks if the player's score is higher than his former 
@@ -101,48 +95,31 @@ def compare_score(name, player_score):
 	with open("saves", "rb") as file:
 		my_depickler = pickle.Unpickler(file)
 		scoreboard = my_depickler.load()
-		for player, point in scoreboard.items():			
+		for player, point in scoreboard.items():
 			if point > best_score:
 				best_score = point
 				best_player = player
-		print("The best score is", best_score, "and is owned by :")
-		for player, point in scoreboard.items():
-			if best_score == point:
-				print("		-", player)
-		if player_score == best_score:
-			print(
-				"\nYou have the highest score :", 
-				best_score,
-				"! Congratulations !"
-				)
-
-def mode_choice():
-	phrase_1 = "\nChoose your mode :\n"
-	phrase_2 = "Hit D to play with duplicates in code.\n"
-	phrase_3 = "Hit N to play without duplicates in code."
-	phrase_4 = "Sorry I did not understand. Please try again."
-	print(phrase_1 + phrase_2 + phrase_3)
-	choice = input("Enter your choice : ")
-	if not choice.isalpha():
-		print(phrase_4)
-		return mode_choice()
-	choice = choice.capitalize()
-	if choice == "D":
-		return "duplicates"
-	elif choice == "N":
-		return "not_duplicates"
-	else :
-		print(phrase_4)
-		return mode_choice()		
+			if best_score != 0:
+				print("The best score is", best_score, "and is owned by :")
+				for player, point in scoreboard.items():
+					if best_score == point:
+						print("		-", player)
+				if player_score == best_score :
+					print(
+						"\nYou have the highest score :", 
+						best_score,
+						"! Congratulations !"
+						)
 
 def exit(name):
 	phrase_1 = "\nTo quit hit Q.\n"
 	phrase_2 = "To change player hit P.\n"
 	phrase_3 = "To continue hit C.\n"
+	phrase_4 = "Sorry I did not understand. Please try again."
 	print(phrase_1 + phrase_2 + phrase_3)
 	entry = input("Enter your choice, only one letter : ")
 	if not entry.isalpha():
-		print("Sorry I did not understand. Please try again.")
+		print(phrase_4)
 		return exit(name)
 	entry = entry.capitalize()
 
@@ -150,10 +127,12 @@ def exit(name):
 		return True, name	
 	elif entry == "P":
 		name = input("What's your name ? ")
-		open_backup(name)
+		open_backup_file(name)
 		return False, name
 	elif entry == "C":
 		return False, name
 	else:
-		print("Sorry I did not understand. Please try again.")
+		print(phrase_4)
 		return exit(name)
+
+
