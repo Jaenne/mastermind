df94e05d035aaa0aa9c37eef2fc38bc0801560b4
Jaenne <59830376+Jaenne@users.noreply.github.com>
2020-01-24 19:41:32 +0100

    Update class_mastermind.py

--- class_mastermind.py
+++ class_mastermind.py
@@ -5,6 +5,9 @@
 from random import randrange
 
 class Mastermind:
+	"""
+	Attributs and methods necessary to resolve the game.
+	"""
 	def __init__(self):
 		self.allowed = ["A", "B", "C", "D", "E", "F", "G", "H"]
 		self.combination = ["", "", "", ""]
@@ -13,50 +16,77 @@ class Mastermind:
 		self.good_places = 0
 		self.good_colors = 0
 		self.mode_choice = "duplicates"
+		self.entry = ""
 
-	def set_mode_choice(self):
-		phrase_1 = "\nChoose your mode :\n"
-		phrase_2 = "Hit D to play with duplicates in code.\n"
-		phrase_3 = "Hit N to play without duplicates in code."
-		phrase_4 = "Sorry I did not understand. Please try again."
-		print(phrase_1 + phrase_2 + phrase_3)
+	def set_mode_choice(self, choice):
+		"""
+		The entry is not a letter.
 
-		choice = input("Enter your choice : ")
+		Entry is upper.
+		The mode choosed is with duplicates.
+			set mode
+		The mode choosed is without duplicates.
+			set mode
+
+		The entry is not "d" or "n"
+
+		Each time, returns : 
+			- mode : True if entry is conform or False if not conform.
+			- and The text will be display.
+		"""		
+		str_1 = "Sorry I did not understand. Please try again."
+		str_2 = "\nThe code consists of 4 letters. There may be duplicates."
+		str_3 = "\nThe code consists of 4 letters, without duplicates."
+		
 		if not choice.isalpha():
-			print(phrase_4)
-			return self.set_mode_choice()
-		choice = choice.capitalize()
+			text = str_1
+			mode = False
+
+		choice = choice.upper()
 		if choice == "D":
 			self.mode_choice = "duplicates"
-			print("\nThe code consists of 4 letters. There may be duplicates.")
+			text = str_2
+			mode = True			
 		elif choice == "N":
 			self.mode_choice = "not_duplicates"
-			print("\nThe code consists of 4 letters, without duplicates.")
-		else :
-			print(phrase_4)
-			return self.set_mode_choice()		
+			text = str_3
+			mode = True
 
-	def set_combination(self, entry):
+		else :
+			text = str_1
+			mode = False
+
+		return mode, text
+	
+	def set_combination(self):
+		"""
+		the entry is converted capitals.
+		Each letter of the entry is stored in the list
+		combination.
+		"""
+		self.entry = (self.entry).upper()
+		for index, letter in enumerate(self.entry):
+			self.combination[index] = self.entry[index]
+								
+
+	def verify_entry(self, entry):
 		""" 
 		Entry must contains 4 letters.
 		These letters are stored in the authorized list.		
-		If the entry is suitable, the entry is converted capitals.
-		Each letter of the entry is stored in the list
-		combination.
+		If the entry is suitable : 
+			- returns True ;
+			- set entry.
 		If the entry is not suitable, because there are too many characters
-		or because the upper letters are not in the authorized list, the 
-		function starts over at the beginning.
+		or because the upper letters are not in the authorized list : 
+			- returns False.
 		"""
-		# Thank you Julien00859
-		while True:			
-			if len(entry) == 4 and all(char in self.allowed for char in entry.upper()):
-				entry = (entry).upper()
-				for index, letter in enumerate(entry):
-					self.combination[index] = entry[index]
-				break
-			print("Your combination must contain 4 characters among allowed letters.")
-				
-						
+		# Thank you Julien00859 !
+		if len(entry) == 4 and all(char in self.allowed for char in entry.upper()):
+			self.entry = entry			
+			return True
+		else :						
+			return False
+	
 	def convert_code(self):
 		"""
 		Each number is converted into a letter among those
@@ -64,39 +94,43 @@ class Mastermind:
 		Each letter is saved in self.code.
 		"""
 		for i, contenu in enumerate(self.code):
-			# Thank you Linek !
+			# Thank you Linek !			
 			self.code[i] = self.allowed[int(contenu)]
-			print(self.code)
+		print(self.code)
 								
 	def set_genererate_code(self):
 		""" 
 		The code is a list of 4 "x".
 		Each x takes a value between 0 and 7.
-		If mode choosed is "not_duplicates":
-			If there is a double, the function starts again from the beginning.
-		Else, the function convert_code is called.
+		If mode choosed is "not_duplicates" And if there is a double :
+			- returns False
+		Else :
+			- the function convert_code is called;
+			- returns True
 		"""		
 		for index, element in enumerate(self.code):
 			number = str(randrange(8))
 			self.code[index] = number
 			element = number
-			if self.mode_choice == "not_duplicates":
-				if self.code.count(element) > 1:
-					return self.set_genererate_code()
+			if self.mode_choice == "not_duplicates" and self.code.count(element) > 1:				
+				return False			
 		self.convert_code()
+		return True
 
 	def set_color_comparison(self):
 		""" 
 		Compares the code and the combination by looking
 		for color matches.
-		- duplicates the combination in backup_combination;
-		For each letter in backup_combination :
-			if the backup_combination letter has more occurrences  
-			than the same letter in the code:
-				- Replaces excess occurrences in backup_combination 
-				with an "x";
-		- compare the backup_combination with the code;
-		- increments the test counter by 1.
+		Duplicates the combination in backup_combination;
+		For each element in backup_combination :
+			For each letter in code:
+				if the backup_combination letter has more occurrences  
+				than the same letter in the code:
+					- Replaces excess occurrences in backup_combination 
+					with an "x";
+			if letter in backup_combination is in the code :
+				- increments self.good_colors by 1.
+		Increments the test counter by 1.
 		"""
 		self.good_colors = 0
 		backup_combination = []
@@ -113,7 +147,7 @@ class Mastermind:
 						
 	def set_places_comparison(self):
 		"""
-		Function that searches for well-placed colors:
+		Searches for well-placed colors:
 		Compares the colors for each similar index of the
 		combination list and code list.
 		saves the number of well-placed colors in good_places. 
@@ -127,38 +161,38 @@ class Mastermind:
 
 	def display_help(self):
 		"""
-		Displays help about game.
+		Help text about the game.
 		"""		
-		phrase_1 = "X means : good color but BAD place.\n"
-		phrase_2 = "V means : good color and GOOD place.\n"	
-		phrase_3 = "You have 12 tries.\n"
-		string_allowed = " ".join(self.allowed)
-		print(phrase_1 + phrase_2 + phrase_3)
-		print(
-			"\n\nBAD place(s)", 
-			"	",
-			"Allowed letters :",
-			string_allowed,
-			"	",
-			"GOOD place(s)\n", 
-			)
+		str_1 = "X means : good color but BAD place.\n"
+		str_2 = "V means : good color and GOOD place.\n"	
+		str_3 = "You have 12 tries.\n"
+		str_4 = "\n\nBAD place(s)"
+		str_5 = "	"
+		str_6 = "Allowed letters : "
+		str_7 = string_allowed = " ".join(self.allowed)
+		str_8= "GOOD place(s)\n"
 
-	def display_result(self):
-		self.good_colors = (self.good_colors - self.good_places)
+		text = (str_1+str_2+str_3+str_4+str_5+str_6+str_7+str_5+str_8)
+		return text
+
+	def display_result(self):		
 		"""
-		Displays in 1 line informations necessary 
+		Text : displays in 1 line informations necessary 
 		for the resolution of the code.
 		"""
-		string_conbination = " ".join(self.combination)
-		print(
-			"X :", self.good_colors, 
-			"				",
-			string_conbination, 
-			"			",
-			"V :", self.good_places, 
-			"		",
-			"try n°", self.tries_counter
-			)
+		self.good_colors = (self.good_colors - self.good_places)
+		str_1 = "X : "
+		str_2 = str(self.good_colors)
+		str_3 = "				"
+		str_4 = " ".join(self.combination)
+		str_5 = "V : "
+		str_6 = str(self.good_places)
+		str_7 = "		"
+		str_8 = "try : "
+		str_9 = str(self.tries_counter)
+		
+		text = str_1+str_2+str_3+str_4+str_3+str_5+str_6+str_7+str_8+str_9
+		return text
 
 	def get_good_places(self):
 		return self.good_places
@@ -168,5 +202,3 @@ class Mastermind:
 
 	def get_code(self):
 		return self.code
-
-
